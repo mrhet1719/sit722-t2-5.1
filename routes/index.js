69d744101b71b5c506d499c76b9b13ef9aa4ed8b
Aiman Shahroz <s222423263@deakin.edu.au>
2023-07-23 21:12:13 +1000

    Initial Project commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const nameList = [ 
-  {'id': 1, 'name': 'John', 'email': 'aaa'},
-  {'id': 2, 'name': 'James', 'email': 'bbb'},
-  {'id': 100, 'name': 'David', 'email': 'ccc'}
-];
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722', sub: 'ABC', names: nameList });
+  res.render('index', { title: 'DevOps SIT722 Glossary', sub: 'Explore my website featuring a curated collection of DevOps terms and their concise descriptions.'});
 });
 
 module.exports = router;
